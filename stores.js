1efd43c3ee8a2ea500560c180dcd68d98e025db3
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-22 11:31:49 -0400

    Galeries Le Gardeur 1.3  /stores.js

--- stores.js
+++ stores.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
         
         for (var x in stores) {
             stores[x].name_locale = stores[x].name;
-            if (sessionStorage.secondary_locale == sessionStorage.current_locale) {
+            if (Cookies.get('secondary_locale') == Cookies.get('current_locale')) {
                 if (stores[x].name_2) {
                     stores[x].name_locale = stores[x].name_2;
                 }
@@ -62,7 +62,7 @@ $(document).ready(function() {
             val.link = "cat" + val.id;
             cat_name = val.name;
             
-            if (sessionStorage.secondary_locale == sessionStorage.current_locale && val.name_2) {
+            if (Cookies.get('secondary_locale') == Cookies.get('current_locale') && val.name_2) {
                 cat_name = val.name_2;
             }
             
@@ -152,7 +152,7 @@ $(document).ready(function() {
 
         for (var x in all_stores) {
             all_stores[x].name_locale = all_stores[x].name;
-            if (sessionStorage.secondary_locale == sessionStorage.current_locale) {
+            if (Cookies.get('secondary_locale') == Cookies.get('current_locale')) {
                 if (all_stores[x].name_2) {
                     all_stores[x].name_locale = all_stores[x].name_2;
                 }
