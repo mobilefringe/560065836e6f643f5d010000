a96abe096a379b0d48271479fff06d7b8cef39f1
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-06-22 11:25:49 -0400

    Galeries Le Gardeur 1.3  /application.js

--- application.js
+++ application.js
@@ -278,28 +278,6 @@ $(document).ready(function() {
 		showSearchResults();
 	});
     
-// 	//Using i18n for localization, for more info please visit //i18next.com/
-// 	i18n.init({preload: [sessionStorage.primary_locale,sessionStorage.secondary_locale],resGetPath: '../__lng__.json',fallbackLng: false }, 
-// 		function(t) {
-// 			var current_local = sessionStorage.primary_locale;
-// 			if(typeof(sessionStorage.current_locale) != 'undefined' ){
-// 				current_local = sessionStorage.current_locale;
-// 			}
-// 			// We're switching french to be the primary locale                
-// 			if(current_local == sessionStorage.primary_locale){
-// 				setPrimaryLanguage();
-// 				$('.en-CA').addClass('langSelect');
-// 				$('.fr-CA').removeClass('langSelect');
-// 			}else{
-// 				setSecondaryLanguage();
-// 				$('.fr-CA').addClass('langSelect');
-// 				$('.en-CA').removeClass('langSelect');
-				
-// 			}
-// 		}
-// 	);
-	
-	
 	//Using i18n for localization, for more info please visit http://i18next.com/
     i18n.init({preload: [getStorage().primary_locale, getStorage().secondary_locale], resGetPath: '../__lng__.json', fallbackLng: false }, function(t) {
         var current_locale = "";
