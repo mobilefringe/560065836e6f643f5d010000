ef2583c554c7495e157db78249c93f0585876a2d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-08-31 14:22:06 -0400

    Galeries Le Gardeur 1.4  /promotions_id.js

--- promotions_id.js
+++ promotions_id.js
@@ -46,10 +46,11 @@ $(document).ready(function() {
         Mustache.parse(template_html);   // optional, speeds up future uses
         item_list.push(collection);
         $.each( item_list , function( key, val ) {
-            if ((val.promo_image_url_abs).indexOf('missing.png') > -1){
+            if ((val.promo_image_url).indexOf('missing.png') > -1){
                 if (val.promotionable_type == "Store") {
                     var store_details = getStoreDetailsByID(val.promotionable_id);
-                    val.alt_promo_image_url = getImageURL(store_details.store_front_url);
+                    // val.alt_promo_image_url = getImageURL(store_details.store_front_url);
+                    val.alt_promo_image_url = val.promo_image_url_abs;
                     val.store_detail_btn = store_details.slug 
                     val.store_name = store_details.name
                 } else {
